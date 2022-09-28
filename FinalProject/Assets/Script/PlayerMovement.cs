3a2ab5874975969f644f6a5c2b3700aad3e331a3
Khoa Tran <tran.dangkhoa047@gmail.com>
2022-09-28 22:57:18 +0700

    git commit

--- FinalProject/Assets/Script/PlayerMovement.cs
+++ FinalProject/Assets/Script/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 10f;
+    private float direct = 0f;
+    float speed = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +15,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        direct = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direct * speed, GetComponent<Rigidbody2D>().velocity.y);
+        Debug.Log(GetComponent<Rigidbody2D>().velocity);
         if (Input.GetKeyDown("space"))
         {
 
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 7);
 
             Debug.Log("Jumped");
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
+        /*        else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }*/
+
     }
 
 }
