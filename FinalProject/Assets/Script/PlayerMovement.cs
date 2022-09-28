5a88bb3022ba9f750734579c7123e0cc36928a77
Nevermine909 <alxarrow123@gmail.com>
2022-09-28 15:20:07 +0700

    Add map and character moving

--- FinalProject/Assets/Script/PlayerMovement.cs
+++ FinalProject/Assets/Script/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+
+            Debug.Log("Jumped");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
     }
+
 }
