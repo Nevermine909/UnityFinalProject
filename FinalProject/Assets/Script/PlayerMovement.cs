6cdf8993f9a86a212f68141bbde4a8f18e579a34
Khoa Tran <tran.dangkhoa047@gmail.com>
2022-10-04 13:27:48 +0700

    all change

--- FinalProject/Assets/Script/PlayerMovement.cs
+++ FinalProject/Assets/Script/PlayerMovement.cs
@@ -4,36 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody;
     private float direct = 0f;
-    float speed = 2f;
+    private float speed = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         direct = Input.GetAxisRaw("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(direct * speed, GetComponent<Rigidbody2D>().velocity.y);
-        Debug.Log(GetComponent<Rigidbody2D>().velocity);
+        rigidbody.velocity = new Vector2(direct * speed, rigidbody.velocity.y);
+        FlipChar(direct);
+        Debug.Log(rigidbody.velocity);
         if (Input.GetKeyDown("space"))
         {
-
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 7);
-
+            rigidbody.velocity = new Vector2(0, 7);
             Debug.Log("Jumped");
         }
-        /*        else if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    transform.Translate(Vector3.left * speed * Time.deltaTime);
-                }
-                else if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    transform.Translate(Vector3.right * speed * Time.deltaTime);
-                }*/
 
+    }
+    void FlipChar(float direct)
+    {
+        if (direct < 0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(direct > 0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0); ;
+        }
     }
 
 }
