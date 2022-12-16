4db8ada916421b89ded34aa10372210f9ca94844
holeejesus <83935096+hellfive123@users.noreply.github.com>
2022-12-16 13:14:29 +0700

    Add BeforeDialogScene

---
+++ FinalProject/Assets/Script/Dialog/Dialog.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialog();
+    }
+
+    // Update is called once per frame
+    public void nextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(WordByWord());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void StartDialog()
+    {
+        index = 0;
+        StartCoroutine(WordByWord());
+    }
+
+    IEnumerator WordByWord()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+}
